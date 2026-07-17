e1cb615875c4701dd36ecf3508572dcfa8cfa831
Ornythorus <jpoitevin42@gmail.com>
2026-07-17 12:34:48 +0200

    feat: initialisation (#2)

---
+++ config.inc.php
@@ -0,0 +1,12 @@
+<?php
+//define('DB_HOST'), 'db');
+//define('DB_NAME', 'gestion-zoo');
+//define('DB_USERNAME', 'root');
+//define('DB_PASSWORD', 'root');
+//define('SITE_URL','http://localhost:8080/gestion-zoo/');
+
+const DB_HOST = 'db';
+const DB_NAME = 'gestion-zoo';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = 'root';
+const SITE_URL = 'http://localhost:8080/gestion-zoo/';
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    require_once("header.php");
+    ?>
+
+<div class="container">
+    <div class="card">
+        <div class="card-body">
+            <h1>Zoologik</h1>
+        </div>
+    </div>
+</div>
+
+<?php
+    require_once("footer.php");
+?>
